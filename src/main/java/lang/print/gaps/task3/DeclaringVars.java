996631984cfb2e97eecd9f00016fa85db3f97ac2
Admin <budda4613@gmail.com>
2022-09-14 21:55:43 +0500

    3.3

--- src/main/java/lang/print/gaps/task3/DeclaringVars.java
+++ src/main/java/lang/print/gaps/task3/DeclaringVars.java
@@ -2,6 +2,11 @@ package lang.print.gaps.task3;
 
 public class DeclaringVars {
     public static void main(String[] args) {
+        int a,b,c;
+
+        a=b=c=10;
+
+        System.out.println(c);
         //write code here
     }
 }
--- src/main/java/lang/print/gaps/task3/ReassigningValues.java
+++ src/main/java/lang/print/gaps/task3/ReassigningValues.java
@@ -2,6 +2,24 @@ package lang.print.gaps.task3;
 
 public class ReassigningValues {
     public static void main(String[] args) {
-        //write code here
+        int first=1;
+        int second=10;
+        int third=100;
+        System.out.println(first); //w
+        System.out.println(second);
+        System.out.println(third);
+
+        first=15;
+        second=16;
+        third=4;
+
+        int linkToFirst=first;
+        int linkToSecond=second;
+        int linkToThird=third;
+
+        System.out.println(linkToFirst); //w
+        System.out.println(linkToSecond);
+        System.out.println(linkToThird);
+
     }
 }
